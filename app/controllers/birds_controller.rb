9c398ed51440d5a57a9feaeaf4c7cd29abbd711a
Mitzi17 <m_estrada17@live.com>
2021-03-31 23:38:14 -0700

    commit tree 16275a3bf2f476237e26b3f6f0a22d41090359bf

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,22 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds, only: [:id, :name, :species]
+    # slice won't work for an array of hashes
+    # so we can use only: or except:
   end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    if bird
+    render json: bird, only: [:id, :name, :species]
+    # can also be written as:
+    # render json: {id: bird.id, name: bird.name, species: bird.species }
+    # or as:
+    # render json: bird.slice(:id, :name, :species)
+  else
+    render json: { message: 'Bird not found' }
+  end
+  end
+
 end
